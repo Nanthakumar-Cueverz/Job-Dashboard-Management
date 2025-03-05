ed3d067c76e9cfccb40e7a96c8c25709189df6f0
Nanthakumar <nanthakumar.cueverz@gmail.com>
2025-03-05 14:40:34 +0530

    Update a apache

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,11 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-    server: {
-        host: true,
-    },
+    // server: {
+    //     host: true,
+    // },
     plugins: [react(), tailwindcss(), svgr()],
+    base: '/interviewai/',
     resolve: {
         alias: {
             '@images': path.resolve(__dirname, './src/assets/images'),
